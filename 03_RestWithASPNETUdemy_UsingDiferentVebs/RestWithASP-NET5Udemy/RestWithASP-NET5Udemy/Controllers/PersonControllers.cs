3590be3d92f01016006636fd740bec46fead8fb7
Nathalia Riegler Schwertner Defavari <nathalia.schwertner@ibm.com>
2022-06-05 17:09:53 -0300

    Adding folder Model

---
+++ 03_RestWithASPNETUdemy_UsingDiferentVebs/RestWithASP-NET5Udemy/RestWithASP-NET5Udemy/Controllers/PersonControllers.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASP_NET5Udemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+       // private bool isNumber;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        //Soma 
+        [HttpGet("sum/{firstNumber}/{secondNumber}")] //path especifico onde vão ser passados os parâmetros
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            return BadRequest("Invalid Input");
+        }
+
+      
+        
+    }
+}
---
+++ 03_RestWithASPNETUdemy_UsingDiferentVebs/RestWithASP-NET5Udemy/RestWithASP-NET5Udemy/Model/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace RestWithASP_NET5Udemy.Model
+{
+    public class Person
+    {
+        public long id { get; set; }
+        public long FirstName { get; set; }
+        public long LastName { get; set; }
+        public long Gender { get; set; }
+    }
+}
