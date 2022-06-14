62f004bf82434a16979975ea47cf9918a9d77871
Nathalia Riegler Schwertner Defavari <nathalia.schwertner@ibm.com>
2022-06-14 11:55:24 -0300

    Layered architecture, adding repository

---
+++ 06_RestWithASPNETUdemy_SplittingLogic/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Model.Context;
+using RestWithASPNETUdemy.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        //declaração MySQLContext
+        private readonly IPersonRepository _repository;
+
+        //construtor
+        public PersonServiceImplementation(IPersonRepository repository)//recebendo a injeção
+        {
+            _repository = repository;  //atribui a variavel ao contex declarado na classe
+        }
+        
+        // Método responsável por devolver todas as pessoas,
+        public List<Person> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+        // Método responsável por devolver uma pessoa por ID
+        public Person FindByID(long id)
+        {
+            return _repository.FindByID(id);
+        }
+        public Person Create(Person person)
+        {
+            return _repository.Create(person);
+        }
+
+        // Método responsável por atualizar uma pessoa
+        public Person Update(Person person)
+        {
+            return _repository.Update(person);
+        }
+
+        // Método responsável por excluir uma pessoa de um ID
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+    
+    }
+}
---
+++ 06_RestWithASPNETUdemy_SplittingLogic/RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RestWithASPNETUdemy.Services;
+using RestWithASPNETUdemy.Model.Context;
+using RestWithASPNETUdemy.Services.Implementations;
+using Microsoft.EntityFrameworkCore;
+using RestWithASPNETUdemy.Repository.Implementations;
+using RestWithASPNETUdemy.Repository;
+
+namespace RestWithASPNETUdemy
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        // Este método é chamado pelo tempo de execução. Use este método para adicionar serviços ao contêiner.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            //acessa aquivo appsettings le as propriedades, encontra a mySQLConnection, depois encontra a
+            //mySQLConnectionString
+            var connection = Configuration["MySQLConnection:MySQLConnectionString"];
+
+            //DdContext
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
+
+            //Versiona a API
+            services.AddApiVersioning();
+
+            //injeção de dependencia
+            services.AddScoped<IPersonService, PersonServiceImplementation>();
+            services.AddScoped<IPersonRepository, PersonRepositoryImplementation>();
+        }
+
+        // Este método é chamado pelo tempo de execução. Use este método para configurar o pipeline de solicitação HTTP.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
