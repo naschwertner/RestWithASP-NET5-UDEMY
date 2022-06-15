c3323814f5d6d304de15df6c6cd896ac77dc360e
Nathalia Riegler Schwertner Defavari <nathalia.schwertner@ibm.com>
2022-06-14 23:09:29 -0300

    Adding Suport to DB Migrations

---
+++ 07_RestWithASPNETUdemy_AddingSupportToDatabaseMigrations/RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
@@ -0,0 +1,105 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RestWithASPNETUdemy.Model.Context;
+using RestWithASPNETUdemy.Business;
+using RestWithASPNETUdemy.Business.Implementations;
+using RestWithASPNETUdemy.Repository;
+using RestWithASPNETUdemy.Repository.Implementations;
+using Serilog;
+using System;
+using System.Collections.Generic;
+namespace RestWithASPNETUdemy
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+        }
+
+
+        // Este método é chamado pelo tempo de execução. Use este método para adicionar serviços ao contêiner.
+        public void ConfigureServices(IServiceCollection services)
+        {
+           
+            services.AddControllers();
+
+            //acessa aquivo appsettings le as propriedades, encontra a mySQLConnection, depois encontra a
+            //mySQLConnectionString
+            var connection = Configuration["MySQLConnection:MySQLConnectionString"];
+            
+            if (Environment.IsDevelopment());
+            {
+                MigrateDatabase(connection);
+            }
+
+            //DdContext
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
+
+            //Versiona a API
+            services.AddApiVersioning();
+
+            //injeção de dependencia
+            services.AddScoped<IPersonService, PersonServiceImplementation>();
+            services.AddScoped<IPersonRepository, PersonRepositoryImplementation>();
+        }
+
+       
+
+
+
+        // Este método é chamado pelo tempo de execução. Use este método para configurar o pipeline de solicitação HTTP.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            }); 
+
+            
+           
+        }
+        //metodo de migration 
+        private void MigrateDatabase(string connection)
+        {
+            try
+            {
+                var evolveConnection = new MySql.Data.MySqlClient.MySqlConnection(connection);
+                var evolve = new Evolve.Evolve(evolveConnection, msg => Log.Information(msg))
+                {
+                    Locations = new List<string> { "db/migrations", "db/dataset" },
+                    IsEraseDisabled = true,
+                };
+                evolve.Migrate();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Database migration failed", ex);
+                throw;
+            }
+        }
+
+    }
+}
