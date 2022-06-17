fefed92faec2414dd7bd3d5af7448a74ea9d4495
Nathalia Riegler Schwertner Defavari <nathalia.schwertner@ibm.com>
2022-06-17 15:55:57 -0300

    Final resolution implementation endpoints "Book"

---
+++ 07_ResluçãoFinalBook_RestWithASPNETUdemy_AddingSupportToDatabaseMigrations/RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/Implementations/PersonRepositorymplementation.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace RestWithASPNETUdemy.Repository.Implementations
+{
+    public class PersonRepositoryImplementation : IPersonRepository
+    {
+        //declaração MySQLContext
+        private MySQLContext _context;
+
+        //construtor
+        public PersonRepositoryImplementation(MySQLContext context)//recebendo a injeção
+        {
+            _context = context;  //atribui a variavel ao contex declarado na classe
+        }
+        
+        // Método responsável por devolver todas as pessoas,
+        public List<Person> FindAll()
+        {
+            return _context.People.ToList();
+        }
+
+        // Método responsável por devolver uma pessoa por ID
+        public Person FindByID(long id)
+        {
+            return _context.People.SingleOrDefault(p => p.Id.Equals(id));
+        }
+        public Person Create(Person person)
+        {
+            //esse try catch salva  objeto depois retorna ele 
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;      
+            }
+            return person;
+        }
+
+        // Método responsável por atualizar uma pessoa
+        public Person Update(Person person)
+        {
+            if (!Exists(person.Id)) return null;
+
+
+            var result = _context.People.SingleOrDefault(p => p.Id.Equals(person.Id));
+            if (result != null){
+
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                    
+            }
+            return person;
+        }
+
+        // Método responsável por excluir uma pessoa de um ID
+        public void Delete(long id)
+        {
+            var result = _context.People.SingleOrDefault(p => p.Id.Equals(id));
+            if (result != null)
+            {
+
+                try
+                {
+                    _context.People.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+            }
+        }
+
+    
+        public bool Exists(long id)
+        {
+            return _context.People.Any(p => p.Id.Equals(id));
+        }
+    }
+}
---
+++ 07_ResluçãoFinalBook_RestWithASPNETUdemy_AddingSupportToDatabaseMigrations/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/BookServiceImplementation.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Model.Context;
+using RestWithASPNETUdemy.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public class BookServiceImplementation : IBookService
+    {
+        //declaração MySQLContext
+        private readonly IBookRepository _repository;
+
+        //construtor
+        public BookServiceImplementation(IBookRepository repository)//recebendo a injeção
+        {
+            _repository = repository;  //atribui a variavel ao contex declarado na classe
+        }
+        
+        // Método responsável por devolver todas as pessoas,
+        public List<Book> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+        // Método responsável por devolver uma pessoa por ID
+        public Book FindByID(long id)
+        {
+            return _repository.FindByID(id);
+        }
+        public Book Create(Book book)
+        {
+            return _repository.Create(book);
+        }
+
+        // Método responsável por atualizar uma pessoa
+        public Book Update(Book book)
+        {
+            return _repository.Update(book);
+        }
+
+        // Método responsável por excluir uma pessoa de um ID
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+    
+    }
+}
