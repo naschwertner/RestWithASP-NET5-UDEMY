722f0ce0781c851977ed7dbab6bdbe15c6ded2bb
Nathalia Riegler Schwertner Defavari <nathalia.schwertner@ibm.com>
2022-07-01 15:56:05 -0300

    Adding CORS Support

---
+++ 14_RestWithASPNETUdemy_CORS/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/IPersonService.cs
@@ -0,0 +1,15 @@
+﻿using RestWithASPNETUdemy.Data.VO;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Services
+{
+    public interface IPersonService
+    {
+        PersonVO Create(PersonVO person);
+        PersonVO FindByID(long id);
+        List<PersonVO> FindAll();
+        PersonVO Update(PersonVO person);
+        void Delete(long id);
+
+    }
+}
